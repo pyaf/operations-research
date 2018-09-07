ff9d957f15ed564eb3970dd0222465f2859982a9
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2018-09-07 20:41:35 +0530

    Bugfixes

--- simplex-method/simplex.py
+++ simplex-method/simplex.py
@@ -3,63 +3,65 @@ import pdb
 from data import data
 from utils import print_table, print_vars, canonicalize, update_basis, update_vars
 
-a = np.asarray(data['a'], dtype='float')
-b = np.asarray(data['b'], dtype='float')
-c = np.asarray(data['c'], dtype='float')
-n = data['num_of_variables']
-n_slack_vars = data['num_of_slack_variables']
-m = data['num_of_constraints']
+a = np.asarray(data["a"], dtype="float")  # contraint coefficients
+b = np.asarray(data["b"], dtype="float")  # value of constraints
+c = np.asarray(data["c"], dtype="float")  # objective function coefficients
+n = data["num_of_variables"]
+n_slack_vars = data["num_of_slack_variables"]
+m = data["num_of_constraints"]
 
 # define variables
 vars = {}
-for i in range(1, n+1):
+for i in range(1, n + 1):
     vars["x" + str(i)] = None
 
 # basis = collection of basic variables (non-zero)
 basis = []
-for i in range(n-n_slack_vars+1, n+1):
+for i in range(n - n_slack_vars + 1, n + 1):
     basis.append("x" + str(i))
 
 
 # initial feasible basic solution
-# slack variables are basic variables, decision variables are non-basic varialbles
-for i in range(1, n-n_slack_vars+1):
+# slack variables are basic variables
+# decision variables are non-basic varialbles
+for i in range(1, n - n_slack_vars + 1):
     vars["x" + str(i)] = 0.0
 
-for i, j in enumerate(range(n-n_slack_vars+1, n+1)):
-    vars["x" + str(j)] = b[i-1, 0]
-
+for i, j in enumerate(range(n - n_slack_vars + 1, n + 1)):
+    vars["x" + str(j)] = b[i, 0]
 
 # check if initial solution is feasible or not!
 for key in vars:
     if vars[key] < 0:
-        print('Non feasible initial solution found')
+        print("Non feasible initial solution found")
         exit()
 
-print('\nInitial feasible basic solution: ')
+print("\nInitial feasible basic solution: ")
 print_vars(vars)
+s = np.argmin(c)  # idx of min cost coefficient
+r = print_table(a, b, c, s, basis, vars)  # the idx min b/a value
 
-# find `s` such that c[s, 0] is min(c)
+i = 1
 while True:
-    cs = np.min(c)
-    if cs >= 0:
-        print('Optimal solution reached.')
-        print_vars(vars)
-        exit()
-
-    s = np.argmin(c)
-    pivot_idx = print_table(a, b, c, s, basis, vars)
-    print(pivot_idx)  # remember matrix is zero indexed
-    # now we have pivot element, we need to modify `basis`
+    input()
+    print("\nIteration number : %d" % i)
+    # we have pivot element at coordinates = [r, s], we need to modify `basis`
     # a basis variable will be converted into non-basis variable.
-    basis = update_basis(a, basis, pivot_idx, s)
+    basis = update_basis(a, basis, r, s)
     # get the table in canonical form
-    a, b, c = canonicalize(a, b, c, pivot_idx, s)
+    a, b, c = canonicalize(a, b, c, r, s)
     # evaluate variables
     vars = update_vars(a, b, vars, basis)
-    print('Solution obtained:')
+    r = print_table(a, b, c, s, basis, vars) # the idx min b/a value
+    if np.min(c) >= 0:  # all cost coefficients are positive
+        print("\n" + "*" * 10 + "Optimal solution reached." + "*" * 10 + "\n")
+        print_vars(vars)
+        exit()
+    if np.min(np.amax(a, axis=0)) <= 0:  # if any column is all negative
+        print("\n" + "*" * 10 + "Solution out of bounds." + "*" * 10 + "\n")
+        exit()
+
+    print("Solution for this iteration:")
     print_vars(vars)
-    # print the updated table
-    print_table(a, b, c, s, basis, vars)
-
-
+    s = np.argmin(c)  # idx of min cost coefficient
+    i += 1
--- simplex-method/utils.py
+++ simplex-method/utils.py
@@ -5,27 +5,27 @@ from beautifultable import BeautifulTable
 
 
 def evaluate_obj_fn(c, vars):
-    '''Evaluates objective function, takes in `c` and `var`'''
-    f = sum([c[i, 0] * vars["x" + str(i+1)] for i in range(c.shape[0])])
+    """Evaluates objective function, takes in `c` and `var`"""
+    f = sum([c[i, 0] * vars["x" + str(i + 1)] for i in range(c.shape[0])])
     return f
 
 
 def calculate_b_a_ratios(a, b, c, s):
     b_a_ratios = []
     for i in range(a.shape[0]):
-        ratio = b[i, 0] / a[i, s] if a[i, s] > 0 else 'inf'
+        ratio = b[i, 0] / a[i, s] if a[i, s] > 0 else "inf"
         b_a_ratios.append(ratio)
-    ratio = b[i+1, 0] / c[s, 0] if c[s, 0] > 0 else 'inf'
+    ratio = b[i + 1, 0] / c[s, 0] if c[s, 0] > 0 else "inf"
     b_a_ratios.append(ratio)
     return b_a_ratios
 
 
 def print_table(a, b, c, s, basis, vars):
-    '''Beautifully prints table'''
+    """Beautifully prints table"""
     try:
         b_a_ratios = calculate_b_a_ratios(a, b, c, s)
         table = BeautifulTable()
-        header = ["x"+str(i) for i in range(1, a.shape[1]+1)]
+        header = ["x" + str(i) for i in range(1, a.shape[1] + 1)]
         header.extend(["-f", "b", "b/a"])
         table.column_headers = ["Basic Variables"] + header
         for i in range(a.shape[0]):  # #equations
@@ -33,12 +33,14 @@ def print_table(a, b, c, s, basis, vars):
             for j in range(a.shape[1]):
                 row.append(a[i, j])
             _f = evaluate_obj_fn(c, vars)
-            row.extend([_f, b[i, 0], b_a_ratios[i]])
+            ratio = '' if b_a_ratios[i] == 'inf' else b_a_ratios[i]  # ignore inf values
+            row.extend([_f, b[i, 0], ratio])
             table.append_row(row)
-        row = ['-f']
+        row = ["-f"]
         for j in range(c.shape[0]):
             row.append(c[j, 0])
-        row.extend([1, b[i+1, 0], b_a_ratios[-1]])
+        ratio = '' if b_a_ratios[-1] == 'inf' else b_a_ratios[i]  # ignore inf values
+        row.extend([1, b[i + 1, 0], ratio])
         table.append_row(row)
         print(table)
         pivot_index = np.argmin(b_a_ratios)
@@ -67,7 +69,7 @@ def canonicalize(a, b, c, x, y):
             continue
         b[i, 0] -= b[x, 0] * a[i, y]
         a[i, :] -= a[x, :] * a[i, y]
-    b[i+1, 0] -= b[x, 0] * c[0, y]
+    b[i + 1, 0] -= b[x, 0] * c[0, y]
     c -= a[x, :] * c[0, y]
     return a, b, c.T
 
@@ -87,7 +89,7 @@ def update_vars(a, b, vars, basis):
 def update_basis(a, basis, pivot_idx, s):
     for i, var in enumerate(basis):
         idx = int(var[1:])
-        if a[pivot_idx][idx-1] == 1:
-            basis[i] = "x" + str(s+1)
+        if a[pivot_idx][idx - 1] == 1:
+            basis[i] = "x" + str(s + 1)
             break
     return basis
